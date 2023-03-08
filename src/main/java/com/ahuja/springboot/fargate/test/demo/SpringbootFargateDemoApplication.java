3dee3d4bdc70468bf28860d7f42e23bbcea8fee0
Manan Ahuja <manan202@gmail.com>
2023-03-08 00:30:54 -0500

    adding port to Sprinboot app

--- src/main/java/com/ahuja/springboot/fargate/test/demo/SpringbootFargateDemoApplication.java
+++ src/main/java/com/ahuja/springboot/fargate/test/demo/SpringbootFargateDemoApplication.java
@@ -7,12 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class SpringbootFargateDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootFargateDemoApplication.class, args);
+		SpringApplication app = new SpringApplication(SpringbootFargateDemoApplication.class);
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", "7000"));
+		app.run(args);
 	}
 	@GetMapping("/ping")
 	public String ping(@RequestParam(value = "name", defaultValue = "World") String name) {
